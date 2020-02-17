c97431d2be3eb86bf98b17a288fc32ac6b10708a
avendo <namankaushikplt@gmil.com>
2020-02-18 00:40:02 +0530

    First Commit

--- lib/App.dart
+++ lib/App.dart
@@ -10,18 +10,8 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
-  List<Workshop> entries = [
-    Workshop('images/bvp.jpg',
-        'BVPIEEE: the student branch of IEEE at Bharati Vidyapeeths College of Engineering'),
-    Workshop('images/bvp1.jpg',
-        'The Robotics and Automation Society (BVPIEEE RAS) is a professional society that supports the development and the exchange of scientific knowledge in the fields '),
-    Workshop('images/bvp2.jpg',
-        'BVPIEEE Computer Society (sometimes abbreviated Computer Society or CS) is a professional society of IEEE. Its purpose and scope is “to advance the theory, practice, and application of computer and information processing science and technology” and the “professional standing of its members.')
-  ];
-  List<Workshop> workshops = [
-    Workshop('images/bvp.jpg', null),
-    Workshop('images/bvp2.jpg', null)
-  ];
+List<Workshop>  entries = [Workshop('images/bvp.jpg', 'BVPIEEE: the student branch of IEEE at Bharati Vidyapeeths College of Engineering'),Workshop('images/bvp1.jpg', 'The Robotics and Automation Society (BVPIEEE RAS) is a professional society that supports the development and the exchange of scientific knowledge in the fields '),Workshop('images/bvp2.jpg','BVPIEEE Computer Society (sometimes abbreviated Computer Society or CS) is a professional society of IEEE. Its purpose and scope is “to advance the theory, practice, and application of computer and information processing science and technology” and the “professional standing of its members.')];
+List<Workshop>  workshops=[Workshop('images/bvp.jpg',null),Workshop('images/bvp2.jpg',null)];
 
   @override
   Widget build(context) {
@@ -33,18 +23,32 @@ class AppState extends State<App> {
         child: new App_Bar(context),
       ),
 
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
+        body: TabBarView(
+
           children: <Widget>[
-            latestnews(),
-            news(),
-            workshopes(),
-            workshopsliding(),
+            SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+
+                children: <Widget>[
+                  latestnews(),
+                   Card(child: news(entries.length-1)),
+                   Card(child: news(entries.length-2)),
+                   Card(child: news(entries.length-3)),
+                  workshopes(),
+                  Card(child: workshopsliding()),
+//                  aboutus(),
+
+
+                ],
+              ),
+            ),
+            Center(child: Text("Upcoming events appear here")),
+            Center(child: Text("Chapters apear here")),
           ],
         ),
-      ),
-    );
+      );
+
   }
 
 
@@ -82,96 +86,95 @@ class AppState extends State<App> {
       child: ListView.builder(
           itemCount: workshops.length,
           scrollDirection: Axis.horizontal,
-          itemBuilder: (BuildContext context, int index) {
-            return Row(
-              children: <Widget>[
-                CircleAvatar(
-                  child: Image.asset('${workshops[index].photoslink}'),
-                  radius: 100,
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 10, right: 10),
-                )
-              ],
-            );
-          }),
-    );
-  }
+          itemBuilder: (BuildContext context,int index){
+          return  Row(
+            children: <Widget>[
+              CircleAvatar(
+            child: Image.asset('${workshops[index].photoslink}'),
+            radius: 100,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10,right: 10),
+          )
+            ],
+          );
+        }),
+      );
 
-  Widget news() {
-    return SizedBox(
-        height: 500,
-        child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index % 2 == 0) {
-                return Container(
-                  height: 150,
-                  margin:
-                      EdgeInsets.only(top: 50, bottom: 50, left: 10, right: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      CircleAvatar(
-                        child: Image.asset(
-                          '${entries[index].photoslink}',
-                          fit: BoxFit.cover,
-                          width: 200,
-                          height: 200,
-                        ),
-                        backgroundColor: Colors.amber,
-                        radius: 80,
-                        foregroundColor: Colors.red,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                      ),
-                      Flexible(
-                        child: Text(
-                          '${entries[index].details}',
-                          textAlign: TextAlign.start,
-                          overflow: TextOverflow.fade,
-                          style: TextStyle(fontSize: 15, color: Colors.black87),
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              } else {
-                return Container(
-                  height: 150,
-                  margin:
-                      EdgeInsets.only(top: 50, bottom: 50, left: 10, right: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Flexible(
-                        child: Text(
-                          '${entries[index].details}',
-                          textAlign: TextAlign.start,
-                          overflow: TextOverflow.fade,
-                          style: TextStyle(fontSize: 15, color: Colors.black87),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 10, right: 10),
-                      ),
-                      CircleAvatar(
-                        child: Image.asset(
-                          '${entries[index].photoslink}',
-                          fit: BoxFit.cover,
-                          width: 200,
-                          height: 200,
-                        ),
-                        backgroundColor: Colors.amber,
-                        radius: 80,
-                        foregroundColor: Colors.red,
-                      ),
-                    ],
-                  ),
-                );
-              }
-            }));
-  }
-}
+    }
+
+    Widget news(int index) {
+      if (index < 0) {
+        return null;
+      }
+
+      if (index % 2 == 0) {
+        return Container(
+          height: 150,
+          margin: EdgeInsets.only(top: 50, bottom: 50, left: 10, right: 10),
+
+
+          child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              CircleAvatar(
+                child: Image.asset('${entries[index].photoslink}',
+                  fit: BoxFit.cover,
+                  width: 200,
+                  height: 200,),
+                backgroundColor: Colors.amber,
+                radius: 80,
+                foregroundColor: Colors.red,
+
+              ), Container(
+                margin: EdgeInsets.only(left: 10, right: 10),
+              )
+              ,
+              Flexible(child: Text(
+                '${entries[index].details}', textAlign: TextAlign.start,
+                overflow: TextOverflow.fade,
+                style: TextStyle(fontSize: 15,
+                    color: Colors.black87),),)
+            ],
+          ),
+
+        );
+      }
+      else {
+        return Container(
+          height: 150,
+          margin: EdgeInsets.only(top: 50, bottom: 50, left: 10, right: 10),
+
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+
+              Flexible(child: Text(
+                '${entries[index].details}', textAlign: TextAlign.start,
+                overflow: TextOverflow.fade,
+                style: TextStyle(fontSize: 15,
+                    color: Colors.black87),),),
+              Container(
+                margin: EdgeInsets.only(left: 10, right: 10),
+              )
+              ,
+              CircleAvatar(
+                child: Image.asset('${entries[index].photoslink}',
+                  fit: BoxFit.cover,
+                  width: 200,
+                  height: 200,),
+                backgroundColor: Colors.amber,
+                radius: 80,
+                foregroundColor: Colors.red,
+
+              ),
+
+
+            ],
+          ),
+
+        );
+      }
+    }}
