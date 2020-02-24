c363fc292a3869ec7c942e2eb259689a877cf670
prateekbhatia1402 <61021932+prateekbhatia1402@users.noreply.github.com>
2020-02-24 23:15:16 +0530

    Update Class_Workshop.dart

--- lib/Class_Workshop.dart
+++ lib/Class_Workshop.dart
@@ -1,7 +1,17 @@
-class Workshop
-{
- String photoslink;
- String details;
 
-  Workshop(this.photoslink,this.details);
+import 'package:bvp_ieee/Class_Event.dart';
+class Workshop extends OurEvent
+{
+  String chapter;
+  List<Mentor> mentors;
+
+  Workshop(String eventNumber,String photoslink,String intro,String date,String venue,String details,String formLink,this.chapter,this.mentors)
+  :super(eventNumber,photoslink,intro,date,venue,details,formLink);
+}
+
+class Mentor{
+  String name;
+  String photolink;
+  Mentor(this.name,this.photolink);
+
 }
